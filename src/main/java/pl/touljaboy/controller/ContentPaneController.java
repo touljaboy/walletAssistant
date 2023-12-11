04fc9867f5bd31d68b27c2836ad90f1511eb5b38
touljaboy <ernest.fudali@gmail.com>
2023-12-11 18:14:00 +0100

    0.2 GUI branch Early Beta Prototype panes were created in order to test the app in a basic GUI environment.

---
+++ src/main/java/pl/touljaboy/controller/ContentPaneController.java
@@ -0,0 +1,30 @@
+package pl.touljaboy.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class ContentPaneController {
+
+    @FXML
+    private Button calendarButton;
+
+    @FXML
+    private Button expenseTypesButton;
+
+    @FXML
+    private Button expensesButton;
+
+    @FXML
+    private Button plotButton;
+
+    @FXML
+    private Button settingsButton;
+
+    @FXML
+    private Button usersButton;
+
+    @FXML
+    private Label welcomeLabel;
+
+}
