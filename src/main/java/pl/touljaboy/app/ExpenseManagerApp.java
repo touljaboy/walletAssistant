c60ef9a0bc6a2793ac6b0218065808fc72e92d8d
touljaboy <ernest.fudali@gmail.com>
2023-12-30 14:32:11 +0100

    10.1 Stable Alpha - App Console Version

--- src/main/java/pl/touljaboy/app/ExpenseManagerApp.java
+++ src/main/java/pl/touljaboy/app/ExpenseManagerApp.java
@@ -3,7 +3,7 @@ package pl.touljaboy.app;
 //For now, I plan to test some basic features using the terminal before I move on to making it a GUI app.
 public class ExpenseManagerApp {
     //Application version field
-    public static final String APP_VERSION = "\nExpensesManager - 10.1 STABLE ALPHA - Console Version";
+    public static final String APP_VERSION = "\nExpensesManager - 10.1 STABLE ALPHA - Console Version Branch";
     public static void main(String[] args) {
         ExpenseAppManager expenseAppManager = new ExpenseAppManager();
         expenseAppManager.userLoginLoop();
